e97abf190b82eceaedd014abed05d194a9400f1d
Arlet <shulga@niuitmo.ru>
2023-11-14 22:53:00 +0300

    Add test data

--- stage-3/documents.sql
+++ stage-3/documents.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS documents(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
-    data_url VARCHAR(256) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    data_url TEXT NOT NULL,
     owner_id INT,
     signed BOOLEAN
 );
@@ -12,3 +12,7 @@ ALTER TABLE ONLY documents
 INSERT INTO documents 
 (name, data_url, owner_id, signed) 
 VALUES('agreement', 'http://unblockfolio.ru/docs/a41ghdf1', NULL, TRUE);
+
+INSERT INTO documents 
+(name, data_url, owner_id, signed) 
+VALUES('passport', 'http://unblockfolio.ru/docs/412dffgs', 0, FALSE);
--- stage-3/employees.sql
+++ stage-3/employees.sql
@@ -7,3 +7,7 @@ CREATE TABLE IF NOT EXISTS employees(
     payment_id INT REFERENCES payments_info ON DELETE SET NULL,
     agreement_id INT REFERENCES documents ON DELETE SET NULL
 );
+
+INSERT INTO employees
+(full_name, job_title, employment_date)
+VALUES ('Matthew Ivanov Sergeevich', 'director of factory', '2003-02-02');

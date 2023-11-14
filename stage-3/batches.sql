16d2698cadf5077faa3d253b408a7dbd6a70c6d2
Arlet <shulga@niuitmo.ru>
2023-11-14 23:18:21 +0300

    fixes

--- stage-3/batches.sql
+++ stage-3/batches.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS batches(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    shift_id INT NOT NULL REFERENCES workshifts ON DELETE CASCADE,
-    pelmeni_type INT NOT NULL REFERENCES pelmeni_types ON DELETE CASCADE,
-    pelmeni_size VARCHAR(50) NOT NULL REFERENCES pelmeni_sizes ON DELETE CASCADE,
-    summary_mass REAL NOT NULL CHECK(summary_mass > 0),
+    shift_id INT REFERENCES workshifts ON DELETE SET NULL,
+    pelmeni_type INT REFERENCES pelmeni_types ON DELETE SET NULL,
+    pelmeni_size VARCHAR(50) REFERENCES pelmeni_sizes ON DELETE SET NULL,
+    summary_mass REAL NOT NULL CHECK(summary_mass >= 0),
     created TIMESTAMP NOT NULL,
-    factory_id INT NOT NULL REFERENCES factories ON DELETE CASCADE,
+    factory_id INT REFERENCES factories ON DELETE SET NULL,
     package_time TIMESTAMP,
-    packs_count INT CHECK(packs_count IS NULL OR packs_count > 0),
-    mass_of_defective REAL CHECK(mass_of_defective IS NULL OR mass_of_defective > 0)
+    packs_count INT CHECK(packs_count IS NULL OR packs_count >= 0),
+    mass_of_defective REAL CHECK(mass_of_defective IS NULL OR mass_of_defective >= 0)
 );
--- stage-3/tech_reports.sql
+++ stage-3/tech_reports.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tech_reports (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    reported_id INT NOT NULL REFERENCES employees ON DELETE CASCADE,
-    shift_id INT NOT NULL REFERENCES workshifts ON DELETE CASCADE,
+    reported_id INT REFERENCES employees ON DELETE SET NULL,
+    shift_id INT REFERENCES workshifts ON DELETE SET NULL,
     state INT NOT NULL CHECK(state BETWEEN 1 AND 5),
     created TIMESTAMP,
     description TEXT

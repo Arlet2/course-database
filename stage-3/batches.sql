92310b4c4d036dcfcd7e9c0b0d89a7091af20ad1
Arlet <shulga@niuitmo.ru>
2023-11-14 23:10:24 +0300

    Fixes

--- stage-3/batches.sql
+++ stage-3/batches.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS batches(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     shift_id INT NOT NULL REFERENCES workshifts ON DELETE CASCADE,
     pelmeni_type INT NOT NULL REFERENCES pelmeni_types ON DELETE CASCADE,
-    pelmeni_size VARCHAR(50) NOT NULL REFERENCES pelmeni_size ON DELETE CASCADE,
-    summary_mass REAL NOT NULL,
+    pelmeni_size VARCHAR(50) NOT NULL REFERENCES pelmeni_sizes ON DELETE CASCADE,
+    summary_mass REAL NOT NULL CHECK(summary_mass > 0),
     created TIMESTAMP NOT NULL,
     factory_id INT NOT NULL REFERENCES factories ON DELETE CASCADE,
     package_time TIMESTAMP,
-    packs_count INT,
-    mass_of_defective REAL
+    packs_count INT CHECK(packs_count IS NULL OR packs_count > 0),
+    mass_of_defective REAL CHECK(mass_of_defective IS NULL OR mass_of_defective > 0)
 );
--- stage-3/payments_info.sql
+++ stage-3/payments_info.sql
@@ -1,7 +1,7 @@
 -- Active: 1698966099613@@127.0.0.1@5432@studs@s338861
 CREATE TABLE IF NOT EXISTS payments_info(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    salary INT NOT NULL,
+    salary INT NOT NULL CHECK (salary >= 0),
     bank_name VARCHAR(100) NOT NULL,
     account_number VARCHAR(50) NOT NULL,
     bik VARCHAR(50),

e8b135c003ec5c11cfb14d54eb1d25dcc25ddca8
Matthew <matthewthedigital@gmail.com>
2023-11-22 00:05:01 +0300

    ABOBA

--- stage-3/BatchDelivery.sql
+++ stage-3/BatchDelivery.sql
@@ -10,6 +10,61 @@ CREATE TABLE IF NOT EXISTS batch_delivery(
     PRIMARY KEY(delivery_id, batch_id)
 );
 
+CREATE OR REPLACE FUNCTION check_batch_delivery_correct_amount() 
+RETURNS trigger AS $batch_delivery_check$
+BEGIN
+    IF (
+        SELECT packs_count FROM batches WHERE id = NEW.batch_id
+    ) IS NULL 
+    THEN
+        RAISE EXCEPTION 'Batch packs_count should be not NULL!';
+    END IF;
+
+    IF (
+        SELECT packs_count FROM batches WHERE id = NEW.batch_id
+    ) - (
+        SELECT SUM(amount) as packs_count FROM batch_delivery WHERE batch_id = NEW.batch_id
+    ) < NEW.amount 
+    THEN
+       RAISE EXCEPTION 'There is no such amount packs in this batch!';
+    END IF;
+
+    IF (
+        SELECT t.packs_capacity FROM deliveries d JOIN transports t 
+            ON d.transport_number = t.transport_number 
+        WHERE d.delivery_id = NEW.delivery_id
+    ) - (
+        SELECT SUM(amount) as packs_count FROM batch_delivery WHERE delivery_id = NEW.delivery_id
+    ) < NEW.amount 
+    THEN
+       RAISE EXCEPTION 'There is not enought transport capacity!';
+    END IF;
+
+    RETURN NEW;
+END;
+$batch_delivery_check$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION update_delivery_packs_count()
+RETURNS trigger AS $delivery_packs_count$
+BEGIN
+    UPDATE deliveries 
+    SET packs_count = (
+        SELECT SUM(amount) as packs_count FROM batch_delivery WHERE delivery_id = NEW.delivery_id
+    ) WHERE delivery_id = NEW.delivery_id;
+END;
+$delivery_packs_count$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE TRIGGER batch_delivery_update_trigger
+    BEFORE INSERT OR UPDATE OF amount ON batch_delivery
+    FOR EACH ROW
+    EXECUTE FUNCTION check_batch_delivery_correct_amount();
+
+CREATE OR REPLACE TRIGGER delivery_packs_count_trigger
+    AFTER INSERT OR DELETE OR UPDATE OF amount ON batch_delivery
+    FOR EACH ROW
+    EXECUTE FUNCTION update_delivery_packs_count();
+
 INSERT INTO batch_delivery 
     (delivery_id, batch_id, amount, mass) 
 VALUES
